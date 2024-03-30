bacc326fd01fa0eafbc66d6a827df234f6a6fb6f
Ivan Lukovkin <121473397+iamlukovkin@users.noreply.github.com>
2024-03-31 00:01:43 +0300

    Updated pom.xml

--- src/main/java/evm/cli/Printer.java
+++
@@ -1,9 +0,0 @@
-package evm.cli;
-
-public class Printer {
-    public static void printGap() {
-        System.out.println();
-        for (int i = 0; i < 100; i++) System.out.print("-");
-        System.out.println("\n");
-    }
-}
